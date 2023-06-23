db971060c54047827e177fc979a08ece14ce632c
Anthony Nuon-Prak <anuonprak@duck.com>
2023-06-22 20:00:44 -0700

    Initial Commit

---
+++ Homework2/Homework2App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework2App.swift
+//  Homework2
+//
+//  Created by Anthony Nuon-Prak on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Homework2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
